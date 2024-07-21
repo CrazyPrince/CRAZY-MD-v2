350dbdcec627d0cd070ca303b05d8d70815bfa83
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-21 12:53:32 +0100

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -1,1 +1,6 @@
-
+//BY CRAZY PRINCE
+const axios = require('axios')
+const { sck1, tiny, fancytext, listall,cmd,ffmpeg } = require('../lib/')
+const fs = require('fs-extra');
+const { exec } = require('child_process')
+const { Sticker, createSticker, StickerTypes } = require("wa-sticker-formatter");

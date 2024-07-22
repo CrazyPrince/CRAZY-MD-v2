1ead2b28b2cd1612056608c5891228383156b01e
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-22 13:43:57 +0100

    Update config.js

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ module.exports = {
   VERSION: process.env.VERSION === undefined ? 'v.0.0.1' : process.env.VERSION,
   LANG: process.env.THEME|| 'CRAZY',
   WORKTYPE: process.env.WORKTYPE === undefined ? 'public' : process.env.WORKTYPE,
-  LG: process.env.LG === undefined ? 'true' : process.env.LG
+  LG: process.env.LG === undefined ? 'false' : process.env.LG
 };
 
 

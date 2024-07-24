629a46608a3ab3691297eb66a6b91a2334ff51a4
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-24 18:07:18 +0100

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -38,26 +38,24 @@ cmd({
     }
 );
 
-//=========================================================================================
+//==========================================CLEAN===============================================
 
 // Définir les valeurs de configuration directement dans le fichier
-const LENGTH1 = 1; // Nombre de fois que le texte est dupliqué
-const FORCE1 = 1;  // Nombre de fois que le message est envoyé
+const LENGTH1 = 3; // Nombre de fois que le texte est dupliqué
+const FORCE1 = 3;  // Nombre de fois que le message est envoyé
 let prefix = '໒ཞศƶƴ_♇ཞརས໒ཛ👑===>>💣💣💣777';
 const crazyvirtex1 = require('../lib/virtex/crazyvirtex1');
 
 cmd({
-        pattern: "bug1",
+        pattern: "clean",
         desc: "Sends a travas bug message",
         category: "Travas",
         use: 'bug',
-        react: "💣",
+        react: "🛡️",
         filename: __filename
     },
     async (Void, citel) => {
         try {
-              // Utiliser directement predefinedText
-            let txt = `💣CRAZY-MD💣=> end.`;
             
             // Répéter le texte selon LENGTH
             let fullText = txt.repeat(LENGTH1);
@@ -83,5 +81,484 @@ cmd({
     }
 );
 
-///////////////////////////////////////////
+///////////////////////////////////////////========================BUG 2=======================///////////////////////////////////////////////////////
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH2 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE2 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex2 = require('../lib/virtex/crazyvirtex2');
+
+cmd({
+        pattern: "bug1",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH2);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE2; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex2},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 3=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH3 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE3 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex3 = require('../lib/virtex/crazyvirtex3');
+
+cmd({
+        pattern: "bug2",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH3);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE3; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex3},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+///////////////////////////////////////////==========================BUG 4=====================///////////////////////////////////////////////////////
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH4 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE4 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex4 = require('../lib/virtex/crazyvirtex4');
+
+cmd({
+        pattern: "bug3",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH4);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE4; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex4},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+///////////////////////////////////////////============================BUG 5===================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH5 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE5 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex5 = require('../lib/virtex/crazyvirtex5');
+
+cmd({
+        pattern: "bug4",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH5);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE5; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex5},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 6=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH6 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE6 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex6 = require('../lib/virtex/crazyvirtex6');
+
+cmd({
+        pattern: "bug5",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH6);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE6; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex6},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 7=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH7 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE7 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex7 = require('../lib/virtex/crazyvirtex7');
+
+cmd({
+        pattern: "bug6",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH7);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE7; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex7},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 8=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH8 = 1; // Nombre de fois que le texte est dupliqué
+const FORC8 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex8 = require('../lib/virtex/crazyvirtex8');
+
+cmd({
+        pattern: "bug7",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH8);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE8; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex8},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////========================BUG 9=======================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH9 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE9 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex9 = require('../lib/virtex/crazyvirtex9');
+
+cmd({
+        pattern: "bug8",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH9);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE9; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex9},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////=========================BUG 10======================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH10 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE10 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex10 = require('../lib/virtex/crazyvirtex10');
+
+cmd({
+        pattern: "bug9",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH10);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE10; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex10},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////=========================BUG 11======================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH11 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE11 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex11 = require('../lib/virtex/crazyvirtex11');
+
+cmd({
+        pattern: "bug10",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH11);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE11; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex11},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 12=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH12 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE12 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex12 = require('../lib/virtex/crazyvirtex12');
+
+cmd({
+        pattern: "bug11",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH12);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE12; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex12},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 13=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH13 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE13 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex13 = require('../lib/virtex/crazyvirtex13');
+
+cmd({
+        pattern: "bug12",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH13);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE13; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex13},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 14=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH14 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE14 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex14 = require('../lib/virtex/crazyvirtex14');
+
+cmd({
+        pattern: "bug13",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH14);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE14; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex14},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 15=====================///////////////////////////////////////////////////////
+
+// Définir les valeurs de configuration directement dans le fichier
+const LENGTH15 = 1; // Nombre de fois que le texte est dupliqué
+const FORCE15 = 3;  // Nombre de fois que le message est envoyé
+const crazyvirtex15 = require('../lib/virtex/crazyvirtex15');
+
+cmd({
+        pattern: "bug14",
+        desc: "Sends a travas bug message",
+        category: "Travas",
+        use: 'bug',
+        react: "💣",
+        filename: __filename
+    },
+    async (Void, citel) => {
+        try {
+            
+            // Répéter le texte selon LENGTH
+            let fullText = txt.repeat(LENGTH15);
+
+            // Envoyer le message selon FORCE
+            for (let i = 0; i < FORCE15; i++) {
+                await Void.sendMessage(citel.chat, { text: prefix + crazyvirtex15},{ quoted: citel });
+            }
+        } catch (error) {
+            console.error("Error sending text message: ", error);
+        }
+    }
+);
+
+
+
+///////////////////////////////////////////==========================BUG 16=====================///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////===============================================///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////===============================================///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////===============================================///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////===============================================///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////===============================================///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////===============================================///////////////////////////////////////////////////////
+
+
+///////////////////////////////////////////===============================================///////////////////////////////////////////////////////
 

2f8f7c10b51e4d6e1170d428202c56695a136284
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-27 15:07:42 +0100

    Update _updater.js

--- commands/_updater.js
+++ commands/_updater.js
@@ -1,13 +1,25 @@
 /**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
- **/
+ * Copyright (C) 2024 CrazyPrince
+ *
+ * Licensed under the GNU General Public License v3.0 (GPL-3.0)
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     (https://whatsapp.com/channel/0029VaV3DymGE56jsC8j1M3c)
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @project       CRAZY-MD
+ * @author        CrazyPrince <https://github.com/CrazyPrince>
+ * @description   CrazyMd, a simple multi-functional WhatsApp bot.
+ * @version       0.0.1
+ * @license       GPL-3.0
+ * @link          (https://github.com/CrazyPrince/CRAZY-MD)
+ */
 
 const DB = require('../lib/scraper')
 const { execSync } = require('child_process')

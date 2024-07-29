e02a9538dc6eac4847010e82efb6fac64c691dfd
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-07-29 09:11:52 +0100

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -862,7 +862,7 @@ async (Void, citel) => {
     
 
     try {
-        const ppUrl = await Void.profilePictureUrl(citel.quoted, 'image');
+        const ppUrl = await Void.profilePictureUrl(citel.quoted.sender, 'image');
         const response = await axios.get(ppUrl, { responseType: 'arraybuffer' });
         const buffer = Buffer.from(response.data, 'binary');
 

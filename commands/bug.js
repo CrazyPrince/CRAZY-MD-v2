eec1f9c1fc5285ae28e0b24737458056e0f45cc5
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-08-02 13:46:12 +0100

    Update bug.js

--- commands/bug.js
+++ commands/bug.js
@@ -910,14 +910,14 @@ async (Void, citel, text, { isCreator }) => {
     return citel.reply('Veuillez fournir un lien.');
   }
 
-  const apiURL = `https://api.maher-zubair.tech/download/alldownload?url= + text`;
+  const apiURL = `https://api.maher-zubair.tech/download/alldownload?url=${encodeURIComponent(text)}`;
 
   try {
     const response = await axios.get(apiURL);
     const { result } = response.data;
     console.log(response.data)
 
-    if (result && result.medias && result.medias.length > 0) {
+    if (result && result.result.medias && result.result.medias.length > 0) {
       const videoUrl = result.result.medias[0].url;;
       const title = result.title;
 

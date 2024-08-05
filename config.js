bea0aa56bb76ad10d42c56bee3d3f2e79433b63c
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-08-05 19:10:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,8 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "237694268225";
+process.env.OWNER_NUMBER = process.env.OWNER_NUMBER || '237694268225'
+global.owner = process.env.OWNER_NUMBER.split(",")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'crazycvv@gmail.com'

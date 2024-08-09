1f70a8d232e7ff578011df741beb6aba3703b964
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-08-09 10:45:45 +0100

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 
 cmd({
   pattern: "yuri",
-  desc: "Récupérer et envoyer une image NSFW Yuri",
+  desc: "Retrieve and send an NSFW image",
   category: "nsfw",
   react: "🧼",
   filename: __filename
@@ -13,14 +13,19 @@ cmd({
 
 async (Void, citel, text, { isCreator }) => {
   const apiURL = `https://api.maher-zubair.tech/nsfw/yuri`;
-
+  
   try {
     const response = await axios.get(apiURL);
     const { url } = response.data;
     console.log(response.data);
 
     if (url) {
-      const msg = `Voici une image Yuri :`;
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
 
       await Void.sendMessage(citel.chat, {
         image: {
@@ -31,10 +36,390 @@ async (Void, citel, text, { isCreator }) => {
         quoted: citel,
       });
     } else {
-      citel.reply('Image non trouvée.');
+      citel.reply('Image not found.');
     }
   } catch (error) {
-    console.error('Erreur lors de la récupération de l\'image :', error);
-    citel.reply('Une erreur est survenue lors de la récupération de l\'image. Veuillez réessayer plus tard.');
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
   }
 });
+//===========================================================================================
+cmd({
+  pattern: "ass",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🧼",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/ass`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "pussy",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🧼",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/pussy`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "lesbian",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🧼",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/lesbian`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "fingering",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🧼",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/fingering`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "dildo",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🧼",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/dildo`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "dick",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🏳️‍⚧️",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/dick`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "bdsm",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🏳️‍⚧️",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/bdsm`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "blowjob",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🏳️‍⚧️",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/blowjob`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "fucks",
+  desc: "Retrieve and send an NSFW image",
+  category: "nsfw",
+  react: "🏳️‍⚧️",
+  filename: __filename
+},
+
+async (Void, citel, text, { isCreator }) => {
+  const apiURL = `https://api.maher-zubair.tech/nsfw/fuck`;
+  
+  try {
+    const response = await axios.get(apiURL);
+    const { url } = response.data;
+    console.log(response.data);
+
+    if (url) {
+      const msg = `╔══════⊰⊱═══════╗
+      𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓢𝓕𝓦
+╚══════⊰⊱═══════╝
+
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+
+      await Void.sendMessage(citel.chat, {
+        image: {
+          url: url,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+    } else {
+      citel.reply('Image not found.');
+    }
+  } catch (error) {
+    console.error('Error when recovering the image:', error);
+    citel.reply('An error occurred while retrieving the image. Please try again later.');
+  }
+});
+//===========================================================================================
+
+//===========================================================================================
+
+//===========================================================================================
+
+//===========================================================================================
+
+//===========================================================================================
+
+//===========================================================================================

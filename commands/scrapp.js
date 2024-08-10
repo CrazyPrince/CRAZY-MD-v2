a4b24e070cacdc5308ca1b4dae1925926a89ffb5
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-08-11 00:28:36 +0100

    Update scrapp.js

--- commands/scrapp.js
+++ commands/scrapp.js
@@ -598,3 +598,147 @@ cmd({
         console.error(error);
     }
 });
+//=====================================================================================================================
+
+cmd({
+  pattern: "gpt",
+  desc: "to generate prompt from OpenAI",
+  category: "AI",
+  react: "🧠",
+  filename: __filename
+},
+
+async (Void, citel, text) => {
+  if (!text) {
+    return citel.reply(`Please provide me a query`);
+  }
+
+  // Liste des mots à vérifier
+  const greetings = ["hi", "hello", "bonjour", "hey", "salut", "hola", "morning", "gn", "gm", "yo", "crazy"];
+  
+  // Vérifier si le texte correspond à l'un des mots (en ignorant la casse)
+  if (greetings.includes(text.toLowerCase())) {
+    return citel.reply('Hi, I am CrazyMd. How can I help you today ? My bruh 🙂‍↕️');
+  }
+
+  try {
+    const apiURL = `https://api.maher-zubair.tech/ai/chatgpt2?q=${encodeURIComponent(text)}`;
+    const response = await axios.get(apiURL);
+    const { result } = response.data;
+    console.log(response.data);
+
+    await Void.sendMessage(citel.chat, { text: 'ᴄʀᴀᴢʏ ɢᴘᴛ: ' + result }, { quoted: citel });
+  } catch (error) {
+    console.error('Error:', error);
+    citel.reply('An error occurred. Please try again later.');
+  }
+});
+
+//===========================================================================================
+cmd({
+  pattern: "math",
+  desc: "to resolve math exercices",
+  category: "AI",
+  react: "🧠",
+  filename: __filename
+},
+
+async (Void, citel, text) => {
+  if (!text) {
+    return citel.reply(`Please provide me a math problem`);
+  }
+
+  // Liste des mots à vérifier
+  const greetings = ["hi", "hello", "bonjour", "hey", "salut", "hola", "morning", "gn", "gm", "yo", "crazy"];
+  
+  // Vérifier si le texte correspond à l'un des mots (en ignorant la casse)
+  if (greetings.includes(text.toLowerCase())) {
+    return citel.reply('Hi, I am CrazyMd. How can I help you today ? My bruh 🙂‍↕️');
+  }
+
+  try {
+    const apiURL = `https://api.maher-zubair.tech/ai/mathssolve?q=${encodeURIComponent(text)}`;
+    const response = await axios.get(apiURL);
+    const { result } = response.data;
+    console.log(response.data);
+
+    await Void.sendMessage(citel.chat, { text: 'ᴄʀᴀᴢʏ ᴍᴀᴛʜ ᴀɪ: ' + result }, { quoted: citel });
+  } catch (error) {
+    console.error('Error:', error);
+    citel.reply('An error occurred. Please try again later.');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "insult",
+  desc: "to generate insults",
+  category: "misc",
+  react: "🤬",
+  filename: __filename
+},
+
+async (Void, citel) => {
+
+  try {
+    const apiURL = `https://api.maher-zubair.tech/misc/insult`;
+    const response = await axios.get(apiURL);
+    const { result } = response.data;
+    console.log(response.data);
+
+    await Void.sendMessage(citel.chat, { text: result}, { quoted: citel });
+  } catch (error) {
+    console.error('Error:', error);
+    citel.reply('An error occurred. Please try again later...');
+  }
+});
+//===========================================================================================
+cmd({
+  pattern: "iosnews",
+  desc: "to get daily ios tech news",
+  category: "news",
+  react: "📰",
+  filename: __filename
+},
+
+async (Void, citel) => {
+
+  try {
+    const apiURL = `https://api.maher-zubair.tech/details/ios`;
+    const response = await axios.get(apiURL);
+    const { result } = response.data;
+    console.log(response.data);
+    
+    const titre = result.title;
+    const img = result.images;
+    const body = result.desc;
+    const lien = result.link;
+    
+    let msg = `╔══════⊰⊱═══════╗
+              𝓒𝓡𝓐𝓩𝓨 𝓜𝓓 𝓝𝓔𝓦𝓢
+╚══════⊰⊱═══════╝
+
+𝓣𝓲𝓽𝓵𝓮 :  ${titre}
+
+𝓛𝓲𝓷𝓴:  ${lien}
+
+______________________________________
+𝓝𝓮𝔀𝓼:  
+${body}
+
+             ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴀᴢʏ-ᴍᴅ²³⁷`;
+    await Void.sendMessage(citel.chat, {
+        image: {
+          url: img,
+        },
+        caption: msg,
+      }, {
+        quoted: citel,
+      });
+  } catch (error) {
+    console.error('Error:', error);
+    citel.reply('An error occurred. Please try again later...');
+  }
+});
+//===========================================================================================
+
+

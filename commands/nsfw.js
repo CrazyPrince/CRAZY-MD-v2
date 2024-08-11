348dcc8a6047575cd2f741fddd2e443102910069
CrazyPrince <86961470+CrazyPrince@users.noreply.github.com>
2024-08-11 02:47:52 +0100

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,3 +1,34 @@
+/**
+
+*     ██████╗██████╗  █████╗ ███████╗██╗   ██╗    ███╗   ███╗██████╗     
+*    ██╔════╝██╔══██╗██╔══██╗╚══███╔╝╚██╗ ██╔╝    ████╗ ████║██╔══██╗    
+*    ██║     ██████╔╝███████║  ███╔╝  ╚████╔╝     ██╔████╔██║██║  ██║    
+*    ██║     ██╔══██╗██╔══██║ ███╔╝    ╚██╔╝      ██║╚██╔╝██║██║  ██║    
+*    ╚██████╗██║  ██║██║  ██║███████╗   ██║       ██║ ╚═╝ ██║██████╔╝    
+*     ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝       ╚═╝     ╚═╝╚═════╝     
+                                                                    
+
+ * Copyright (C) 2024 CrazyPrince
+ *
+ * Licensed under the GNU General Public License v3.0 (GPL-3.0)
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     (https://whatsapp.com/channel/0029VaV3DymGE56jsC8j1M3c)
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @project       CRAZY-MD
+ * @author        CrazyPrince <https://github.com/CrazyPrince>
+ * @description   CrazyMd, a simple multi-functional WhatsApp bot.
+ * @version       0.0.1
+ * @license       GPL-3.0
+ * @link          (https://github.com/CrazyPrince/CRAZY-MD)
+ */
 
 const { sck1, tiny, fancytext, cmd } = require('../lib/')
 const axios = require('axios')
